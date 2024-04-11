5e3156c077c0c01e229e48ed0a8e578e0fa5e232
nptri <phuctri1510@gmail.com>
2024-04-11 16:59:43 +0900

    update new api

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+// import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
 export const options = {
     scenario:{
@@ -9,23 +9,27 @@ export const options = {
 }
 
 export default function () {
-    let correlation_id = uuidv4();
-    const url_token = ``;
-    // sleep(1);
+
+    // let correlation_id = uuidv4();
+    const url_endpoint = `https://reqres.in/api/users`;
+
     const payload_body = JSON.stringify({
-      
+        "name": "morpheus",
+        "job": "leader"
     });
+
     const param_header = {
       headers: {
         'Content-Type': 'application/json',
       },
     };
   
-    const req_Token = http.post(url_token, payload_body, param_header);
-    console.log(req_Token.body);
-    check(req_Token, {
-      "response code was 200": (req_Token) => req_Token.status == 200,
-      "Body contains Token": (req_Token) => req_Token.body.includes('token')
+    const req_Package = http.post(url_endpoint, payload_body, param_header);
+    console.log(req_Package.body);
+    check(req_Package, {
+      "response code was 201": (r) => req_Package.status == 201,
+      "Body contains Data": (r) => req_Package.body.includes(`"name":"morpheus"`)
     }
     );
+
 }
